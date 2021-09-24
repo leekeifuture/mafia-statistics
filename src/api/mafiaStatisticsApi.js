247b11d3152ffc32a8518d93bf5603b15d2847f4
Vladislav Brezovsky <opnoe09@gmail.com>
2021-09-24 19:55:01 +0300

    Add file for API calls

---
+++ src/api/mafiaStatisticsApi.js
@@ -0,0 +1,21 @@
+import * as axios from 'axios'
+
+const baseURL = 'http://localhost:8080/api'
+
+const axiosInstance = axios.create({baseURL})
+
+export const mafiaStatisticsApi = {
+    uploadNumbersStatistics(file, statisticsType) {
+        const formData = new FormData()
+        formData.append('file', file, file.name)
+
+        return axiosInstance.post('/upload/excel', formData, {
+            params: {statisticsType: statisticsType.toUpperCase()}
+        })
+            .then(response => response.data)
+    },
+    getPlayerById(id) {
+        return axiosInstance.get(`/players/${id}`)
+            .then(response => response.data)
+    }
+}
