159063b59f1d40e6c2c0f193c34c496ed564da4b
Vladislav Brezovsky <opnoe09@gmail.com>
2021-09-29 14:35:34 +0300

    Export tables to another components

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,17 +1,13 @@
 import withStyles from '@material-ui/core/styles/withStyles'
-import Language from '@material-ui/icons/Language'
 import dashboardStyle
     from 'assets/jss/material-dashboard-pro-react/views/dashboardStyle'
-import Card from 'components/Card/Card.jsx'
-import CardBody from 'components/Card/CardBody.jsx'
-import CardHeader from 'components/Card/CardHeader.jsx'
-import CardIcon from 'components/Card/CardIcon.jsx'
 import GridContainer from 'components/Grid/GridContainer.jsx'
 import GridItem from 'components/Grid/GridItem.jsx'
 import PropTypes from 'prop-types'
 import React from 'react'
-import Table from '../../components/Table/Table'
+import RatingTableComponent from './RatingTableComponent'
 import RecordsComponent from './RecordsComponent'
+import TopGamesTableComponent from './TopGamesTableComponent'
 
 class Dashboard extends React.Component {
     constructor(props) {
@@ -20,137 +16,18 @@ class Dashboard extends React.Component {
     }
 
     render() {
-        const tableHead = [
-            'Место',
-            'Никнейм',
-            'Кол-во игр'
-        ]
-        const tableData = [
-            [
-                '#1',
-                'г-н Плесень',
-                1795
-            ],
-            [
-                '#2',
-                'г-н БИ-2',
-                1407
-            ],
-            [
-                '#3',
-                'г-н Лютер',
-                1362
-            ],
-            [
-                '#4',
-                'г-н Гагарин',
-                1246
-            ],
-            [
-                '#5',
-                'г-н Ауткаст',
-                1236
-            ],
-            [
-                '#6',
-                'г-жа Ягодка',
-                1221
-            ],
-            [
-                '#7',
-                'г-жа Andolini',
-                1126
-            ],
-            [
-                '#8',
-                'г-н Шкипер',
-                1114
-            ],
-            [
-                '#9',
-                'г-жа Ultimate',
-                1064
-            ],
-            [
-                '#10',
-                'г-н Партизан',
-                1038
-            ],
-            [
-                '#11',
-                'г-н ГитлерОк',
-                974
-            ],
-            [
-                '#12',
-                'г-н Белка',
-                949
-            ],
-            [
-                '#13',
-                'г-н Бродяга',
-                947
-            ],
-            [
-                '#14',
-                'г-н Эльф',
-                837
-            ],
-            [
-                '#15',
-                'г-н Старый',
-                828
-            ]
-        ]
-
         const {classes} = this.props
         return (
             <div>
-                <RecordsComponent classes={classes} />
+                <GridContainer>
+                    <RecordsComponent classes={classes} />
+                </GridContainer>
                 <GridContainer>
                     <GridItem xs={12} sm={6} md={6} lg={6}>
-                        <Card>
-                            <CardHeader color="success" icon>
-                                <CardIcon color="success">
-                                    <Language />
-                                </CardIcon>
-                                <h4 className={classes.cardIconTitle}>
-                                    Топ по количеству игр
-                                </h4>
-                            </CardHeader>
-                            <CardBody>
-                                <GridContainer justify="space-between">
-                                    <GridItem xs={12} sm={12} md={12}>
-                                        <Table
-                                            tableHead={tableHead}
-                                            tableData={tableData}
-                                        />
-                                    </GridItem>
-                                </GridContainer>
-                            </CardBody>
-                        </Card>
+                        <TopGamesTableComponent classes={classes} />
                     </GridItem>
                     <GridItem xs={12} sm={6} md={6} lg={6}>
-                        <Card>
-                            <CardHeader color="success" icon>
-                                <CardIcon color="success">
-                                    <Language />
-                                </CardIcon>
-                                <h4 className={classes.cardIconTitle}>
-                                    Рейтинговая таблица
-                                </h4>
-                            </CardHeader>
-                            <CardBody>
-                                <GridContainer justify="space-between">
-                                    <GridItem xs={12} sm={12} md={12}>
-                                        <Table
-                                            tableHead={tableHead}
-                                            tableData={tableData}
-                                        />
-                                    </GridItem>
-                                </GridContainer>
-                            </CardBody>
-                        </Card>
+                        <RatingTableComponent classes={classes} />
                     </GridItem>
                 </GridContainer>
             </div>
---
+++ src/views/Dashboard/TopGamesTableComponent.jsx
@@ -0,0 +1,119 @@
+import Language from '@material-ui/icons/Language'
+import React from 'react'
+import Card from '../../components/Card/Card'
+import CardBody from '../../components/Card/CardBody'
+import CardHeader from '../../components/Card/CardHeader'
+import CardIcon from '../../components/Card/CardIcon'
+import GridContainer from '../../components/Grid/GridContainer'
+import GridItem from '../../components/Grid/GridItem'
+import Table from '../../components/Table/Table'
+
+const TopGamesTableComponent = (props) => {
+    const tableHead = [
+        'Место',
+        'Никнейм',
+        'Кол-во игр'
+    ]
+    const tableData = [
+        [
+            '#1',
+            'г-н Плесень',
+            1795
+        ],
+        [
+            '#2',
+            'г-н БИ-2',
+            1407
+        ],
+        [
+            '#3',
+            'г-н Лютер',
+            1362
+        ],
+        [
+            '#4',
+            'г-н Гагарин',
+            1246
+        ],
+        [
+            '#5',
+            'г-н Ауткаст',
+            1236
+        ],
+        [
+            '#6',
+            'г-жа Ягодка',
+            1221
+        ],
+        [
+            '#7',
+            'г-жа Andolini',
+            1126
+        ],
+        [
+            '#8',
+            'г-н Шкипер',
+            1114
+        ],
+        [
+            '#9',
+            'г-жа Ultimate',
+            1064
+        ],
+        [
+            '#10',
+            'г-н Партизан',
+            1038
+        ],
+        [
+            '#11',
+            'г-н ГитлерОк',
+            974
+        ],
+        [
+            '#12',
+            'г-н Белка',
+            949
+        ],
+        [
+            '#13',
+            'г-н Бродяга',
+            947
+        ],
+        [
+            '#14',
+            'г-н Эльф',
+            837
+        ],
+        [
+            '#15',
+            'г-н Старый',
+            828
+        ]
+    ]
+
+    return (
+        <Card>
+            <CardHeader color="success" icon>
+                <CardIcon color="success">
+                    <Language />
+                </CardIcon>
+                <h4 className={props.classes.cardIconTitle}>
+                    Топ по количеству игр
+                </h4>
+            </CardHeader>
+            <CardBody>
+                <GridContainer justify="space-between">
+                    <GridItem xs={12} sm={12} md={12}>
+                        <Table
+                            tableHead={tableHead}
+                            tableData={tableData}
+                        />
+                    </GridItem>
+                </GridContainer>
+            </CardBody>
+        </Card>
+    )
+}
+
+export default TopGamesTableComponent
