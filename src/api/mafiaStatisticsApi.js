a49b0958e7a1d49325bdcc2a676f35bd9d6ac011
Vladislav Brezovsky <opnoe09@gmail.com>
2021-11-13 15:08:51 +0300

    Return localhost as base url

--- src/api/mafiaStatisticsApi.js
+++ src/api/mafiaStatisticsApi.js
@@ -1,7 +1,7 @@
 import * as axios from 'axios'
 
 export const ACCESS_TOKEN = 'accessToken'
-export const API_BASE_URL = 'https://fe67-178-120-62-224.ngrok.io'
+export const API_BASE_URL = 'http://localhost:8080'
 export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
 
 const axiosInstance = axios.create({
