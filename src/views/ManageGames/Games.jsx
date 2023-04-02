61f167ffe0d5f768775da1953cf4c05d6318b79a
Vladislav Brezovsky <opnoe09@gmail.com>
2023-04-02 18:13:07 +0300

    Add basic games table

---
+++ src/views/ManageGames/Games.jsx
@@ -0,0 +1,147 @@
+// @material-ui/core components
+import withStyles from '@material-ui/core/styles/withStyles'
+// @material-ui/icons
+import {cardTitle} from 'assets/jss/material-dashboard-pro-react.jsx'
+// core components
+import GridContainer from 'components/Grid/GridContainer.jsx'
+import GridItem from 'components/Grid/GridItem.jsx'
+import React from 'react'
+// react component for creating dynamic tables
+import LoadingIndicator
+    from '../../components/LoadingIndicator/LoadingIndicator'
+import GamesComponent from './GamesComponent'
+
+const styles = {
+    cardIconTitle: {
+        ...cardTitle,
+        marginTop: '15px',
+        marginBottom: '0px'
+    }
+}
+
+class Games extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            games: [{
+                'host': {
+                    'id': 123,
+                    'nickname': 'Джек Николаевич Воскресенский'
+                },
+                'number': 1,
+                'bestMove': [
+                    1,
+                    2,
+                    3
+                ],
+                'startDatetime': '2023-03-17T15:13:30',
+                'endDatetime': '2023-02-05T11:51:07',
+                'status': 'COMPLETED',
+                'won': 'RED',
+                'note': 'Some Note',
+                'blackPlayerOne': {
+                    'id': 1,
+                    'nickname': 'Джек Николаевич Воскресенский'
+                },
+                'blackPlayerTwo': {
+                    'id': 3,
+                    'nickname': 'Джек Николаевич Воскресенский'
+                },
+                'donPlayer': {
+                    'id': 31,
+                    'nickname': 'Джек Николаевич Воскресенский'
+                },
+                'sheriffPlayer': {
+                    'id': 5,
+                    'nickname': 'Джек Николаевич Воскресенский'
+                },
+                'firstShootPlayer': {
+                    'id': 7,
+                    'nickname': 'Джек Николаевич Воскресенский'
+                },
+                'players': [
+                    {
+                        'player': {
+                            'id': 1,
+                            'nickname': 'Джек Николаевич Воскресенский'
+                        },
+                        'foulsCount': 3
+                    },
+                    {
+                        'player': {
+                            'id': 1,
+                            'nickname': 'Джек Николаевич Воскресенский'
+                        },
+                        'foulsCount': 3
+                    }
+                ],
+                'bestPlayers': [
+                    {
+                        'player': {
+                            'id': 1,
+                            'nickname': 'Джек Николаевич Воскресенский'
+                        },
+                        'additionalPoints': 0
+                    },
+                    {
+                        'player': {
+                            'id': 7,
+                            'nickname': 'Джек Николаевич Воскресенский'
+                        },
+                        'additionalPoints': 0
+                    }
+                ],
+                'days': [
+                    {
+                        'number': 1,
+                        'votingMap': [
+                            {
+                                'player': {
+                                    'id': 1,
+                                    'nickname': 'Джек Николаевич Воскресенский'
+                                },
+                                'whoPutToVote': {
+                                    'id': 3,
+                                    'nickname': 'Джек Николаевич Воскресенский'
+                                },
+                                'firstVoteCount': 32,
+                                'secondVoteCount': 4
+                            }
+                        ]
+                    }
+                ]
+            }],
+            isLoading: false // TODO: fix loading
+        }
+    }
+
+    // componentDidMount() {
+    //     trackPromise(
+    //         mafiaStatisticsApi.getAllGames()
+    //             .then(
+    //                 data => this.setState({games: data}),
+    //                 error => this.props.history.push('/auth/error')
+    //             )
+    //     ).then(r => this.setState({isLoading: false}))
+    // }
+
+    render() {
+        const {classes} = this.props
+        return (<>
+            {this.state.isLoading
+                ? <LoadingIndicator />
+                : (<>
+                    <GridContainer>
+                        <GridItem xs={12}>
+                            <GamesComponent
+                                classes={classes}
+                                games={this.state.games}
+                            />
+                        </GridItem>
+                    </GridContainer>
+                </>)}
+        </>)
+    }
+}
+
+export default withStyles(styles)(Games)
---
+++ src/views/ManageGames/GamesComponent.jsx
@@ -0,0 +1,141 @@
+import Assignment from '@material-ui/icons/Assignment'
+import React from 'react'
+import ReactTable from 'react-table'
+import Card from '../../components/Card/Card'
+import CardBody from '../../components/Card/CardBody'
+import CardHeader from '../../components/Card/CardHeader'
+import CardIcon from '../../components/Card/CardIcon'
+import {getReadableDate} from '../../util/util'
+
+const GamesComponent = (props) => {
+    const columns = [
+        {
+            Header: 'Ведущий',
+            accessor: 'host'
+        },
+        {
+            Header: 'Победа',
+            accessor: 'won'
+        },
+        {
+            Header: 'Номер',
+            accessor: 'number'
+        },
+        {
+            Header: 'Статус',
+            accessor: 'status'
+        },
+        {
+            Header: 'Дата',
+            accessor: 'date'
+        }
+        // {
+        //     Header: 'Actions',
+        //     accessor: 'actions',
+        //     sortable: false,
+        //     filterable: false
+        // }
+    ]
+
+    const data = props.games.map((game, key) => {
+        return {
+            id: key,
+            host: game.host.nickname ? game.host.nickname : '',
+            won: getTeamWon(game.won),
+            number: game.number,
+            status: getGameStatus(game.status),
+            date: getReadableDate(game.startDatetime, true)
+            // actions: (
+            //     // we've added some custom button actions
+            //     <div className="actions-right">
+            //         {/* use this button to add a like kind of action */}
+            //         <Button
+            //             justIcon
+            //             round
+            //             simple
+            //             onClick={() => {
+            //                 let obj = this.state.data.find(o => o.id === key)
+            //                 alert(
+            //                     'You\'ve clicked LIKE button on \n{ \nName: ' +
+            //                     obj.name +
+            //                     ', \nposition: ' +
+            //                     obj.position +
+            //                     ', \noffice: ' +
+            //                     obj.office +
+            //                     ', \nage: ' +
+            //                     obj.age +
+            //                     '\n}.'
+            //                 )
+            //             }}
+            //             color="info"
+            //             className="like"
+            //         >
+            //             <Favorite />
+            //         </Button>{' '}
+            //         {/* use this button to remove the data row */}
+            //         <Button
+            //             justIcon
+            //             round
+            //             simple
+            //             onClick={() => {
+            //                 var data = this.state.data
+            //                 data.find((o, i) => {
+            //                     if (o.id === key) {
+            //                         // here you should add some custom code so you can delete the data
+            //                         // from this component and from your server as well
+            //                         data.splice(i, 1)
+            //                         return true
+            //                     }
+            //                     return false
+            //                 })
+            //                 this.setState({data: data})
+            //             }}
+            //             color="danger"
+            //             className="remove"
+            //         >
+            //             <Close />
+            //         </Button>{' '}
+            //     </div>
+            // )
+        }
+    })
+
+    return (
+        <Card>
+            <CardHeader color="primary" icon>
+                <CardIcon color="primary">
+                    <Assignment />
+                </CardIcon>
+                <h4 className={props.classes.cardIconTitle}>
+                    Список всех игр
+                </h4>
+            </CardHeader>
+            <CardBody>
+                <ReactTable
+                    columns={columns}
+                    data={data}
+                    filterable
+                    defaultPageSize={10}
+                    showPaginationTop
+                    showPaginationBottom={false}
+                    className="-striped -highlight"
+                />
+            </CardBody>
+        </Card>
+    )
+}
+
+const getTeamWon = wonEnum => {
+    if (wonEnum === 'RED') {
+        return 'Красные'
+    } else if (wonEnum === 'BLACK') {
+        return 'Чёрные'
+    } else if (wonEnum === 'DRAW') {
+        return 'Ничья'
+    }
+}
+
+const getGameStatus = (statusEnum) =>
+    statusEnum === 'COMPLETED' ? 'Завершена' : 'В процессе'
+
+export default GamesComponent
